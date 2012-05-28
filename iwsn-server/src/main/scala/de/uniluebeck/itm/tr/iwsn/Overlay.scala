39f8574787257872484b697dc3ac815dd92b18df
Daniel Bimschas <daniel@bimschas.com>
2012-05-28 17:48:05 +0200

    Akka overlay now works as a proof-of-concept

--- iwsn-server/src/main/scala/de/uniluebeck/itm/tr/iwsn/Overlay.scala
+++ iwsn-server/src/main/scala/de/uniluebeck/itm/tr/iwsn/Overlay.scala
@@ -2,27 +2,34 @@ package de.uniluebeck.itm.tr.iwsn
 
 import java.util.concurrent.{TimeUnit, Executors}
 import akka.actor._
-import akka.event.Logging
+import akka.pattern.ask
+import akka.dispatch.Await
+import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 import collection.immutable._
+import collection.mutable.{Map => MutableMap, HashMap => MutableHashMap}
 import util.Random
 import com.weiglewilczek.slf4s.Logging
 
+// TODO change strings to URNs!!!
+
 sealed trait OverlayMessage
 
-abstract case class RpcRequest(to: Set[String], id: Int) extends OverlayMessage
+abstract case class RpcRequest(to: Set[String], requestId: Int) extends OverlayMessage
 
-abstract case class RpcStatus(from: Set[String], id: Int) extends OverlayMessage
+abstract case class RpcStatus(from: Set[String], requestId: Int) extends OverlayMessage
 
-abstract case class RpcResponse(from: Set[String], id: Int) extends OverlayMessage
+abstract case class RpcResponse(from: Set[String], requestId: Int) extends OverlayMessage
 
-case class FlashNodesRequest(override val to: Set[String], override val id: Int, binaryImage: Array[Byte])
-  extends RpcRequest(to, id)
+case class FlashNodesRequest(override val to: Set[String], override val requestId: Int, binaryImage: Array[Byte])
+  extends RpcRequest(to, requestId)
 
-case class FlashNodesStatus(override val from: Set[String], override val id: Int, progress: Int, msg: Option[String])
-  extends RpcStatus(from, id)
+case class FlashNodesStatus(override val from: Set[String], override val requestId: Int, progress: Int,
+                            msg: Option[String])
+  extends RpcStatus(from, requestId)
 
-case class FlashNodesResponse(override val from: Set[String], override val id: Int) extends RpcResponse(from, id)
+case class FlashNodesResponse(override val from: Set[String], override val requestId: Int)
+  extends RpcResponse(from, requestId)
 
 case class DevicesAttachedEvent(urnsAttached: Set[String]) extends OverlayMessage
 
@@ -34,9 +41,25 @@ case class DownstreamMessage(to: Set[String], payload: Array[Byte]) extends Over
 
 class PortalActor extends Actor with Logging {
 
+  class RpcState(val request: RpcRequest, val client:ActorRef) {
+
+    private var responsesReceivedFrom = Set[String]()
+
+    def receive(response: RpcResponse): RpcState = {
+      responsesReceivedFrom = responsesReceivedFrom ++ response.from
+      this
+    }
+
+    def isComplete = request.to.forall(
+      requestedFrom => {
+        responsesReceivedFrom.contains(requestedFrom)
+      }
+    )
+  }
+
   var devices: Map[ActorRef, Set[String]] = Map()
 
-  var requests: Map[Int, ActorRef] = Map()
+  var requests: Map[Int, RpcState] = Map()
 
   private def gateway = sender
 
@@ -64,17 +87,26 @@ class PortalActor extends Actor with Logging {
         val (gateway, gatewayUrns) = entry
         val intersection = (request.to & gatewayUrns)
         if (!intersection.isEmpty) {
-          gateway ! new FlashNodesRequest(intersection, request.id, request.binaryImage)
+          gateway ! new FlashNodesRequest(intersection, request.requestId, request.binaryImage)
         }
       })
 
     case status: FlashNodesStatus =>
       logger.debug(status.toString)
-      requests.get(status.id).map(clientRef => clientRef ! status)
+      requests.get(status.requestId).map(state => state.client ! status)
 
-    case response: RpcResponse =>
+    case response: FlashNodesResponse =>
       logger.debug(response.toString)
-      requests.get(response.id).map(clientRef => clientRef ! response)
+      // TODO kaputt!
+      requests.get(response.requestId).map(state => {
+        state.receive(response)
+        if (state.isComplete) {
+          logger.debug("Request " + response.requestId + " is complete!")
+          state.client ! new FlashNodesResponse(state.request.to, state.request.requestId)
+          requests = requests - state.request.requestId
+        }
+      })
+      // TODO handle timeouts
 
   }
 }
@@ -86,10 +118,10 @@ class GatewayActor(val portalActor: ActorRef) extends Actor with Logging {
     case request: FlashNodesRequest =>
       logger.debug(request.toString)
       for (i <- 0 until 99) {
-        Thread.sleep(10)
-        sender ! new FlashNodesStatus(request.to, request.id, i, None)
+        Thread.sleep(100)
+        sender ! new FlashNodesStatus(request.to, request.requestId, i, None)
       }
-      sender ! new FlashNodesResponse(request.to, request.id)
+      sender ! new FlashNodesResponse(request.to, request.requestId)
 
     case event: DevicesAttachedEvent =>
       logger.debug(event.toString)
@@ -99,6 +131,10 @@ class GatewayActor(val portalActor: ActorRef) extends Actor with Logging {
       logger.debug(event.toString)
       portalActor ! event
 
+  }
+
+  def test() {
+    println("test")
   }
 }
 
@@ -125,39 +161,25 @@ object Portal extends App with Logging {
 
     logger.info("Portal actor (" + portalActor + ") started!")
 
-    Thread.sleep(4000)
-
-    object requester extends Actor with Logging {
-
-      protected def receive = {
-        case status: FlashNodesStatus =>
-          logger.debug(status.toString)
-        case response: FlashNodesResponse =>
-          logger.debug(response.toString)
-      }
-
-      def request() {
-        portalActor ! new FlashNodesRequest(
-          Set("A, B, C"),
-          new Random().nextInt(),
-          Array.ofDim(1)
-        )
-      }
-    }
-
     val scheduler = Executors.newScheduledThreadPool(1)
     scheduler.scheduleAtFixedRate(new Runnable {
       def run() {
-        requester.request()
+        implicit val timeout = Timeout(15, TimeUnit.SECONDS)
+        val future = portalActor ? new FlashNodesRequest(
+          Set("A", "B", "C"),
+          new Random().nextInt(),
+          Array.ofDim(1)
+        )
+        val result = Await.result(future, timeout.duration).asInstanceOf[FlashNodesResponse]
+        println(result)
       }
-    }, 3, 30, TimeUnit.SECONDS
+    }, 15, 15, TimeUnit.SECONDS
     )
 
   }
 }
 
-object Gateway extends App with Logging {
-
+object Gateway1 extends App with Logging {
 
   override def main(args: Array[String]) {
 
@@ -180,6 +202,37 @@ object Gateway extends App with Logging {
     val gatewayActor = actorSystem.actorOf(Props(new GatewayActor(portalActor)), "gatewayActor")
 
     logger.info("Gateway actor (" + gatewayActor + ") started!")
+
+    gatewayActor ! new DevicesAttachedEvent(Set("A", "B"))
+
+  }
+}
+
+object Gateway2 extends App with Logging {
+
+  override def main(args: Array[String]) {
+
+    de.uniluebeck.itm.tr.util.Logging.setDebugLoggingDefaults()
+
+    val actorSystem = ActorSystem(
+      "gatewayActorSystem",
+      ConfigFactory.parseString(
+        """
+        akka.actor.provider        = "akka.remote.RemoteActorRefProvider"
+        akka.remote.transport      = "akka.remote.netty.NettyRemoteTransport"
+        akka.remote.netty.hostname = "localhost"
+        akka.remote.netty.port     = 3456
+        """
+      )
+    )
+
+    // actorSystem.eventStream.setLogLevel(Logging.DebugLevel)
+    val portalActor = actorSystem.actorFor("akka://portalActorSystem@localhost:1234/user/portalActor")
+    val gatewayActorRef = actorSystem.actorOf(Props(new GatewayActor(portalActor)), "gatewayActor")
+
+    logger.info("Gateway actor (" + gatewayActorRef + ") started!")
+
+    gatewayActorRef ! new DevicesAttachedEvent(Set("C"))
 
   }
 }
