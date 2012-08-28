fe9cbad305a6b40cb4b4afc0b4f68cc9c4c5199c
Sebastian Ebers <ebers@itm.uni-luebeck.de>
2012-08-28 17:35:13 +0200

    Adaptions to TRJPARealm.java (not yet working)

--- snaa-server/src/main/java/de/uniluebeck/itm/tr/snaa/shiro/TRJPARealm.java
+++ snaa-server/src/main/java/de/uniluebeck/itm/tr/snaa/shiro/TRJPARealm.java
@@ -3,10 +3,15 @@ package de.uniluebeck.itm.tr.snaa.shiro;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+
+import de.uniluebeck.itm.tr.snaa.shiro.entity.Roles;
+import de.uniluebeck.itm.tr.snaa.shiro.entity.Users;
 
 public class TRJPARealm extends AuthorizingRealm {
     
@@ -15,28 +20,30 @@ public class TRJPARealm extends AuthorizingRealm {
     
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) throws AuthenticationException {
         UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
-//        UsersEntity user = userDAO.findUser(token.getUsername());
-//        if( user != null ) {
-//            return new SimpleAuthenticationInfo(user.getId(), user.getPassword(), getName());
-//        } else {
+        // TODO: The entity manager has to be injected
+        GenericDao<Users, String> userDao = new GenericDaoImpl<Users, String>() {};
+        Users user = userDao.find(token.getUsername());
+        if( user != null ) {
+            return new SimpleAuthenticationInfo(user.getName(), user.getPassword(), getName());
+        } else {
             return null;
-//        }
+        }
     }
 
 
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-        Long userId = (Long) principals.fromRealm(getName()).iterator().next();
-//        UsersEntity user = userDAO.getUser(userId);
-//        if( user != null ) {
-//            SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-//            for( Role role : user.getRoles() ) {
-//                info.addRole(role.getName());
-//                info.addStringPermissions( role.getPermissions() );
-//            }
-//            return info;
-//        } else {
+        String userId = (String) principals.fromRealm(getName()).iterator().next();
+        Users user = new GenericDaoImpl<Users, String>() {}.find(userId);
+        if( user != null ) {
+            SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+            for( Roles role : user.getRoleses() ) {
+                info.addRole(role.getName());
+//                info.addStringPermissions( role.getPermissionses() );
+            }
+            return info;
+        } else {
             return null;
-//        }
+        }
     }
 
 }
