a5f01a1b29b555ccdc0e238457fc7c41c39767ad
Daniel Bimschas <daniel@bimschas.com>
2012-09-26 14:49:42 +0200

    Added main classes for gateway (IWSNGateway) and portal (IWSNPortal)

---
+++ iwsn-server/src/main/java/de/uniluebeck/itm/tr/iwsn/gateway/IWSNGateway.java
@@ -0,0 +1,9 @@
+package de.uniluebeck.itm.tr.iwsn.gateway;
+
+public class IWSNGateway {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ iwsn-server/src/main/java/de/uniluebeck/itm/tr/iwsn/portal/IWSNPortal.java
@@ -0,0 +1,9 @@
+package de.uniluebeck.itm.tr.iwsn.portal;
+
+public class IWSNPortal {
+
+	public static void main(String[] args) {
+
+	}
+
+}
